23c368f16ee3f420a078399b8c059350fcd00770
Messi_G <negash4@gmail.com>
2021-07-21 20:44:11 -0500

    objects in array

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("arrays and loops intro");
+//console.log("arrays and loops intro");
+
+const group5 = [
+    {
+    id:1,
+    name:"Derek B",
+    email: "dbaylor@gmail.com",
+    favoriteColor: "green",
+    somethingUnigue: "excellent vision",
+}, 
+{
+
+    id:2,
+    name:"Derek M",
+    email: "laxmalone@gmial.com",
+    favoriteColor: "blue",
+    somethingUnigue: "I am bald"
+
+},
+{
+    id:3,
+    name:"Grace",
+    email: "rgs1690@gmail.com",
+    favoriteColor: "green",
+    somethingUnigue: " fraternal twins"
+
+},
+
+{
+    id:4,
+    name:"Meseret",
+    email: "negash4@gmail.com",
+    favoriteColor: "turmeric yellow",
+    somethingUnigue: "negash4@gmail.com"
+
+}]
+
